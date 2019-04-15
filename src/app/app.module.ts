f8c00f212e590b4db52ff98bf4337a57c1882ffe
jbranjith <onlineranjithsekar@gmail.com>
2019-04-15 21:57:25 +0530

    1 file modified, 4 added, 4 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { PropertyBindingComponent } from '../data-binding/property-binding.component';
-import {EventBindingComponent} from '../data-binding/event-binding.component';
+import { PropertyBindingComponent } from '../binding-property/property-binding.component';
+import {EventBindingComponent} from '../binding-event/event-binding.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
