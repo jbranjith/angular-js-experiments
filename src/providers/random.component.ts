afdc159bf0c0d865b349669ba6fa2a48993c20c0
jbranjith <onlineranjithsekar@gmail.com>
2019-04-16 15:52:57 +0530

    1 file modified

--- src/providers/random.component.ts
+++ src/providers/random.component.ts
@@ -2,19 +2,21 @@ import { Component, OnInit } from '@angular/core';
 
 import { RandomService } from 'random.service';
 import { GreetingService } from '../providedin/greeting.service';
+
 @Component({
   selector: 'random',
   template: 'Message: {{message}} <br> <p>{{randomService.random}}</p>',
   providers: [RandomService]
 })
-export class RandomComponent implements OnInit{
+export class RandomComponent implements OnInit {
   message: any;
+
   constructor(
     private randomService: RandomService,
     private greetingService: GreetingService
   ) { }
 
-  ngOnInit(){
-    this.message = this.greetingService.evening();
+ ngOnInit() {
+    this.message = this.greetingService.morning();
   }
 }
