2300cf598c2b0109dc39bc7167694fb9d2e1d413
jbranjith <onlineranjithsekar@gmail.com>
2019-04-17 18:03:48 +0530

    1 file modified

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Product } from './product';
 import { PRODUCTS } from './product.constants';
 
 @Injectable({ providedIn: 'root' })
 export class ProductService {
+
+  constructor(private http: HttpClient) { }
 
   getAllProducts(): Product[] {
     return PRODUCTS;
