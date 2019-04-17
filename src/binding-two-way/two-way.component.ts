b4ceccb5793f027a66cfab7d4a523aa7e2556522
jbranjith <onlineranjithsekar@gmail.com>
2019-04-17 13:39:21 +0530

    2 files modified

--- src/binding-two-way/two-way.component.ts
+++ src/binding-two-way/two-way.component.ts
@@ -1,15 +1,16 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'two-way-binding',
   templateUrl: './two-way.component.html',
   styleUrls: ['./two-way.component.css']
 })
-export class TwoWayBindingComponent{
+export class TwoWayBindingComponent {
   componentMessage: string = "This is TwoWayBindingComponent";
-  name: string ="BindingExample";
-
-  clickMe(){
+  name: string = "BindingExample";
+  address: string = "Chennai";
+  
+  clickMe() {
     alert("clicked me??");
   }
 
